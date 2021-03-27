5eb2646e9a00febef43000c3759a94e6847a8058
dragon314 <kylem31415@users.noreply.github.com>
2021-03-27 01:29:43 -0700

    Kyle Minilab/Blueprint

---
+++ kyleminilab/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, render_template
+kyleminilab_bp = Blueprint('kyleminilab', __name__,
+                            template_folder='templates',)
+@kyleminilab_bp.route('/')
+def index():
+    return render_template("kyleminilab.html")
