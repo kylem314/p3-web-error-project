304c137bdeed483c37ca4d8ba57c1630823c9aac
Bob1437 <72892023+Bob1437@users.noreply.github.com>
2021-04-05 21:49:31 -0700

    Update classb.py

--- jamesminilab/classb.py
+++ jamesminilab/classb.py
@@ -18,7 +18,8 @@ class Battleship:
     def hp(self):
         print(20)
 
-
+    def list_info(self):
+        print("Damage: " + self.Damage + " Health: " + str(self.Health) + " Name: " + self.Name)
 
 
 b1 = Battleship('12', HP, 'Destroyer')
@@ -27,5 +28,15 @@ b1.get_health(HP)
 
 print(f" Health = {b1.get_health(HP)} ")
 
+
+info = []
+info.append(Battleship('12', HP, 'Destroyer'))
+
+print(info)
+
+
+var1 = b1.list_info()
+
+print(var1)
 
 #b1.hp()
