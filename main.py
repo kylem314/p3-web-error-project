5bedf3c03a613674e0ad70db2dd017bcdab42423
GingerTC <72882312+GingerTC@users.noreply.github.com>
2021-04-08 11:12:10 -0700

    added blueprint url

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from aidanminilab import aidanminilab_bp
+from tylerminilab import tylerminilab_bp
 
 from blueprints import Blueprints
 
@@ -7,6 +8,7 @@ from blueprints import Blueprints
 
 app = Flask(__name__)
 app.register_blueprint(aidanminilab_bp, url_prefix='/aidanminilab')
+app.register_blueprint(tylerminilab_bp, url_prefix='/tylerminilab')
 
 #def margindef():
 #    marginx=0
