6cb0c91605a9e43c9c3f026410f12e94bb251e74
Calvin Ni <61429026+statsprojectlol@users.noreply.github.com>
2021-04-26 21:25:36 -0700

    built stuff for strings

--- minilabs/calvinminilab/bubblesort.py
+++ minilabs/calvinminilab/bubblesort.py
@@ -1,19 +1,5 @@
-# Python program for implementation of Bubble Sort
-"""
-def bubbleSort(arr):
-    n = len(arr)
-    # Traverse through all array elements
-    for i in range(n):
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j+1] :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-"""
 class BubbleSort:
-    def __init__(self, input_list):
+    def __init__(self, input_list,isString):
 
         # init/declare the variables
         self.input_list = input_list
@@ -25,6 +11,7 @@ class BubbleSort:
         # init the functions
         self.bubbleSort(input_list)
         # self.OuputList()
+        self.isString = isString
 
     def bubbleSort(self, arr):
         n = len(arr)
@@ -40,18 +27,50 @@ class BubbleSort:
                 # than the next element
                 if arr[j] > arr[j+1] :
                     arr[j], arr[j+1] = arr[j+1], arr[j]
+    def stringSort(self, arr):
+        alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'j', 'K', 'L',
+                 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X',
+                 'Y','Z']
+        for j in range(0, len(arr)):
+            for i in range(0, len(arr)):
+                _sorted = False
+                if i != len(arr)-1:
+                    for k in range(0,len(arr[i])):
+                        if not _sorted:
+                            if k != (len(arr[i]) and len(arr[i+1])):
+                                if alpha.index(arr[i][k]) > alpha.index(arr[i+1][k]):
+                                    arr[i], arr[i+1] = arr[i+1], arr[i]
+                                    _sorted = True
+                                elif alpha.index(arr[i][k]) < alpha.index(arr[i+1][k]):
+                                    _sorted = True
+                                else:
+                                    if len(arr[i+1]) < len(arr[i]):
+                                        arr[i], arr[i+1] = arr[i+1], arr[i]
+                                        _sorted = True
+    def Stringconvert(self,arr):
+        for j in range(0, len(arr)):
+            arr[j] = arr[j].upper()
+
+
     @property
     def OuputList(self):
         arr = self.input_list
-        self.bubbleSort(arr)
-        for i in range(len(arr)):
-            self._output_list.append(arr[i])
-        return self._output_list
+        if(self.isString):
+            self.Stringconvert(arr)
+            self.stringSort(arr)
+            for i in range(len(arr)):
+                self._output_list.append(arr[i])
+            return self._output_list
+        else:
+            self.bubbleSort(arr)
+            for i in range(len(arr)):
+                self._output_list.append(arr[i])
+            return self._output_list
 
 # Driver code to test above
 if __name__ == "__main__":
-    arr = [64, 34, 25, 12, 22, 11, 90]
-    bubble = BubbleSort(arr)
+    arr = [1, 4, 2, 6]
+    bubble = BubbleSort(arr,False)
     print(bubble.OuputList)
     # sorted = []
 #
