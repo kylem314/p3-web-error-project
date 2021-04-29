b2801cd284333d1df50a378ba5991cff8cf3cd34
Calvin Ni <61429026+statsprojectlol@users.noreply.github.com>
2021-04-29 16:15:07 -0700

    further organization and edits to other files to compensate

--- minilabs/calvinminilab/app.py
+++ minilabs/calvinminilab/app.py
@@ -1,9 +1,7 @@
 from flask import request, render_template
-from minilabs.calvinminilab.minilab import Meat
-from minilabs.calvinminilab.bubblesort import BubbleSort
+from minilabs.calvinminilab.algo.minilab import Meat
+from minilabs.calvinminilab.algo import bubblesort
 from minilabs.calvinminilab import calvinminilab_bp
-
-
 
 
 @calvinminilab_bp.route('/',methods = ["GET","POST"])
@@ -16,27 +14,29 @@ def meatgrinder():
         return render_template("meat.html", Meat =Meat(int(request.form.get("series"))))
     return render_template("meat.html", Meat=Meat(1))
 
-@calvinminilab_bp.route('/bubbleSort',methods = ["GET","POST"])
-def bubbleSort():
-    arr = []
-    isString = False
+@calvinminilab_bp.route('/bubblesort', methods=["GET", "POST"])
+def bubblesort():
     if request.form:
-        string = request.form.get("string")
-        arr = string.split()
-        original = string.split()
-        if(request.form["select"] == "integer"):
-            try:
-                for j in range (0,len(arr)):
-                    arr[j] = int(arr[j])
-                for j in range (0,len(original)):
-                    original[j] = int(original[j])
-                return render_template("bubblesort.html",output_list = BubbleSort(arr,isString).OuputList,original_list = original)
-            except ValueError:
-                return render_template("bubblesort.html",output_list = "Can Only Contain String or Integer",original_list = "Error")
-        else:
-            try:
-                isString = True
-                return render_template("bubblesort.html",output_list = BubbleSort(arr,isString).OuputList,original_list = original)
-            except ValueError:
-                return render_template("bubblesort.html",output_list = "Can Only Contain String or Integer",original_list = "Error")
-    return render_template("bubblesort.html",output_list = BubbleSort(arr,isString).OuputList,original_list = arr)
+        all_list = []
+        b = 1 # to ensure first number is 0
+
+        newbox_counter = request.form.get('newbox_counter')
+        print('number of boxes added' +str(newbox_counter))
+
+        numberToItterate = 5 + int(newbox_counter)
+        # iterating through all of the form text fields input
+        for i in range(numberToItterate):
+            string_used = 'user_input' + str(b)
+            user_input = request.form.get(string_used)
+            all_list.append(int(user_input))
+            b = b + 1
+
+        print(all_list)
+        bubble = bubblesort(all_list)
+        return render_template("bubblesort.html", output_list = bubble.OuputList)
+
+    # conversion = Conversion('', all_list)
+    # return render_template("colin/conversion.html", user_input=user_input, conversion=conversion,
+    # list_conversion=conversion._list ,active_page='colin', type='multi', type_js=json.dumps('multi'))
+
+    return render_template("bubblesort.html", active_page='calvin')
