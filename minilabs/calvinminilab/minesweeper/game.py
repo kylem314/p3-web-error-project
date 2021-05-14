220ae0580ff213d8090cce9699a8214311fe3e48
Calvin Ni <61429026+statsprojectlol@users.noreply.github.com>
2021-05-13 21:24:00 -0700

    finished minesweeper off console

--- minilabs/calvinminilab/minesweeper/game.py
+++ minilabs/calvinminilab/minesweeper/game.py
@@ -1,45 +1,75 @@
 import pygame
+from piece import Piece
+from board import Board
 import os
+from solver import Solver
+from time import sleep
 
-class Game():
-    def __init__(self, board, screenSize):
-        self.board = board
-        self.screenSize = screenSize
-        self.pieceSize = self.screenSize[0] / self.board.getSize()[1],self.screenSize[1]/self.board.getSize()[0]
-        self.loadimages
+class Game:
+    def __init__(self, size, prob):
+        self.board = Board(size, prob)
+        pygame.init()
+        self.sizeScreen = 800, 800
+        self.screen = pygame.display.set_mode(self.sizeScreen)
+        self.pieceSize = (self.sizeScreen[0] / size[1], self.sizeScreen[1] / size[0])
+        self.loadPictures()
+        self.solver = Solver(self.board)
+
+    def loadPictures(self):
+        self.images = {}
+        imagesDirectory = "images"
+        for fileName in os.listdir(imagesDirectory):
+            if not fileName.endswith(".png"):
+                continue
+            path = imagesDirectory + r"/" + fileName
+            img = pygame.image.load(path)
+            img = img.convert()
+            img = pygame.transform.scale(img, (int(self.pieceSize[0]), int(self.pieceSize[1])))
+            self.images[fileName.split(".")[0]] = img
 
     def run(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode(self.screenSize)
         running = True
         while running:
             for event in pygame.event.get():
-                if (event.type == pygame.QUIT):
+                if event.type == pygame.QUIT:
                     running = False
-                self.draw()
+                if event.type == pygame.MOUSEBUTTONDOWN and not (self.board.getWon() or self.board.getLost()):
+                    rightClick = pygame.mouse.get_pressed(num_buttons=3)[2]
+                    self.handleClick(pygame.mouse.get_pos(), rightClick)
+                if event.type == pygame.KEYDOWN:
+                    self.solver.move()
+            self.screen.fill((0, 0, 0))
+            self.draw()
             pygame.display.flip()
+            if self.board.getWon():
+                self.win()
+                running = False
         pygame.quit()
 
     def draw(self):
-        topLeft = (0,0)
-        for row in range(self.board.getSize()[0]):
-            for col in range(self.board.getSize):
-                piece = self.board.getPiece((row,col))
-                image = self.getImage(piece)
+        topLeft = (0, 0)
+        for row in self.board.getBoard():
+            for piece in row:
+                rect = pygame.Rect(topLeft, self.pieceSize)
+                image = self.images[self.getImageString(piece)]
                 self.screen.blit(image, topLeft)
                 topLeft = topLeft[0] + self.pieceSize[0], topLeft[1]
-            topLeft = 0, topLeft[1] + self.pieceSize[1]
+            topLeft = (0, topLeft[1] + self.pieceSize[1])
 
-    def loadImages(self):
-        self.images = {}
-        for fileName in os.listdir("images"):
-            if (not fileName.endswith("png")):
-                continue
-            image = pygame.image.load(r"images/" + fileName)
-            image = pygame.transform.scale(image, self.pieceSize)
-            self.images[fileName.split(".")[0]] = image
+    def getImageString(self, piece):
+        if piece.getClicked():
+            return str(piece.getNumAround()) if not piece.getHasBomb() else 'bomb-at-clicked-block'
+        if (self.board.getLost()):
+            if (piece.getHasBomb()):
+                return 'unclicked-bomb'
+            return 'wrong-flag' if piece.getFlagged() else 'empty-block'
+        return 'flag' if piece.getFlagged() else 'empty-block'
 
-    def getImage(self,piece):
-        string = "unclicked-bomb" if piece.getHasBomb() else "empty-block"
+    def handleClick(self, position, flag):
+        index = tuple(int(pos // size) for pos, size in zip(position, self.pieceSize))[::-1]
+        self.board.handleClick(self.board.getPiece(index), flag)
 
-        return self.images[string]
+    def win(self):
+        sound = pygame.mixer.Sound('win.wav')
+        sound.play()
+        sleep(3)
--- minilabs/calvinminilab/minesweeper/piece.py
+++ minilabs/calvinminilab/minesweeper/piece.py
@@ -1,14 +1,42 @@
-class Piece():
+class Piece:
+    # States: Not clicked, clicked, flagged
     def __init__(self, hasBomb):
         self.hasBomb = hasBomb
+        self.around = 0
         self.clicked = False
         self.flagged = False
+        self.neighbors = []
+
+    def __str__(self):
+        return str(self.hasBomb)
+
+    def getNumAround(self):
+        return self.around
 
     def getHasBomb(self):
-        return hasbomb
+        return self.hasBomb
 
     def getClicked(self):
-        return clicked
+        return self.clicked
 
     def getFlagged(self):
-        return flagged
+        return self.flagged
+
+    def toggleFlag(self):
+        self.flagged = not self.flagged
+
+    def handleClick(self):
+        self.clicked = True
+
+    def setNumAround(self):
+        num = 0
+        for neighbor in self.neighbors:
+            if neighbor.getHasBomb():
+                num += 1
+        self.around = num
+
+    def setNeighbors(self, neighbors):
+        self.neighbors = neighbors
+
+    def getNeighbors(self):
+        return self.neighbors
